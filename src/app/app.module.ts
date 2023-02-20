d4402099c4297435edd6f5e09a4319fdbf631257
Konstantin Mirsaitov <myrsaitov@gmail.com>
2023-02-20 17:43:12 +0300

    Добавлена главная страница в роутер

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoServicesModule } from './modules/todo-services/todo-services.module';
+import {RouterModule} from "@angular/router";
+import {ListComponent} from "./modules/todo-services/pages/list/list.component";
 
 @NgModule({
   declarations: [
@@ -11,6 +13,9 @@ import { TodoServicesModule } from './modules/todo-services/todo-services.module
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: ListComponent }
+    ]),
     AppRoutingModule,
     TodoServicesModule
   ],
