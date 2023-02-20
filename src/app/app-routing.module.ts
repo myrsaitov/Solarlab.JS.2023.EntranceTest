e13160e6cb20d414fdfeb5d31d81d22782235597
Konstantin Myrsaitov <myrsaitov@gmail.com>
2023-02-21 00:44:10 +0300

    Список категорий

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,14 @@ import {ListComponent} from "./modules/todo-services/pages/list/list.component";
 import {ViewComponent} from "./modules/todo-services/pages/view/view.component";
 
 const routes: Routes = [
-  { path: '', component: ListComponent },
-  { path: 'todos/:id', component: ViewComponent },
- // { path: 'wel', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+  { path: '', pathMatch: 'full', redirectTo: '/all' },
+  { path: 'all', component: ListComponent },
+  { path: 'in_work', component: ListComponent },
+  { path: 'completed', component: ListComponent },
+  { path: 'deleted', component: ListComponent },
+  { path: 'todos/:id', component: ViewComponent }
+
+ // { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
 ];
 
 @NgModule({
