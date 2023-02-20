cb1c8f22e2d980bcbeaa54ab3202c777db3d05fd
Konstantin Mirsaitov <myrsaitov@gmail.com>
2023-02-20 17:33:26 +0300

    Сгенерирован компонент карточки todo

--- src/app/modules/todo-services/todo-services.module.ts
+++ src/app/modules/todo-services/todo-services.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { TodoServicesRoutingModule } from './todo-services-routing.module';
 import { ListComponent } from './pages/list/list.component';
+import { TodoCardComponent } from './components/todo-card/todo-card.component';
 
 
 @NgModule({
   declarations: [
-    ListComponent
+    ListComponent,
+    TodoCardComponent
   ],
   imports: [
     CommonModule,
