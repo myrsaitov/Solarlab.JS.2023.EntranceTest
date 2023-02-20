7d8c9232e4c23a2623bcd885978a4cac46388b10
Konstantin Mirsaitov <myrsaitov@gmail.com>
2023-02-20 17:30:25 +0300

    Добавлен модуль функционала списка дел

---
+++ src/app/modules/todo-services/todo-services.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TodoServicesRoutingModule } from './todo-services-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TodoServicesRoutingModule
+  ]
+})
+export class TodoServicesModule { }
