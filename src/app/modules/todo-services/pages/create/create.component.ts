49f7b738b4ce0c7a3eb06b4dbc7d866cdbd6efea
Konstantin Mirsaitov <myrsaitov@gmail.com>
2023-02-22 12:08:43 +0300

    Добавлена форма ввода для Todo

--- src/app/modules/todo-services/pages/create/create.component.ts
+++ src/app/modules/todo-services/pages/create/create.component.ts
@@ -2,6 +2,8 @@ import { Component,OnInit } from '@angular/core';
 import {ITodo} from "../../models/todo/i-todo";
 import { ActivatedRoute } from '@angular/router';
 import { TodoService } from './../../services/todo.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { TodoStatus } from '../../enums/todo-status';
 
 @Component({
   selector: 'app-create',
@@ -11,17 +13,49 @@ import { TodoService } from './../../services/todo.service';
 export class CreateComponent {
   id!: number;
   todo!: ITodo;
+  form!: FormGroup;
 
   constructor(
+    public readonly fb: FormBuilder,
     public activatedRoute: ActivatedRoute,
     public todoService: TodoService
     ) { }
 
   ngOnInit() {
+
+    // Валидаторы
+    this.form = this.fb.group({
+        title: ['', [Validators.required, Validators.maxLength(100)]],
+        body: ['', [Validators.required, Validators.maxLength(1000)]]
+        });
+
     this.todoService.loadData();
     this.id = +this.activatedRoute.snapshot.params['id'];
     this.todoService.getTodoById(this.id).subscribe(response => {
       this.todo = response as ITodo;
     });
+  }
+
+  // Возвращает значение c формы соответсвующего поля
+  get title() { return this.form.get('title'); }
+  get body() { return this.form.get('body'); }
+
+  // Нажатие на кнопку "Добавить объявление"
+  submit()
+  {
+
+    // Если форма неправильно заполнена
+    if (this.form.invalid) {
+        return;
+      }
+
+    // Создает DTO объявления
+    const model: Partial<ITodo> = {
+        title: this.title?.value,
+        body: this.body?.value,
+        status: TodoStatus.InWork
+      };
+
+
   }
 }
